43567b43b2af99d83c169cb4a57ccf9d4aea78ff
Apisitkitti <apisit.o@mail.kmutt.ac.th>
2023-10-24 17:22:10 +0700

    add shift dash button

--- Assets/Script/BloodSkill.cs
+++ Assets/Script/BloodSkill.cs
@@ -3,15 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BloodSkill : MonoBehaviour
-{
-  [SerializeField]Attack atk;
-  [SerializeField]Player player;
-  public int Hp_regain =0;
-  public int skill_dam = 0;
-    public void AttackUpGrade()
-    {
-      atk.damage = skill_dam;
-      player.Health += Hp_regain;
-    }
-}
+  public class BloodSkill : MonoBehaviour
+  {
+    [SerializeField]Attack atk;
+    [SerializeField]Player player;
+    public int Hp_regain =0;
+    public int skill_dam = 0;
+      public void AttackUpGrade()
+      {
+        atk.damage = skill_dam;
+        player.Health += Hp_regain;
+      }
+    //   void Update()
+    // {
+    //     if (player.Health <= 0 && player.GetComponent<Player>() != null)
+    //     {
+    //         player.EndMegaAttack();
+    //         Destroy(gameObject);
+    //     }
+    // }
+  }
---
+++ Assets/Script/Tetsuga.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tetsuga : MonoBehaviour
+{
+    [SerializeField] private GameObject WavePrefab;
+    [SerializeField] private Transform attackpo;
+    void PerformWaveAttack()
+    {
+        // Instantiate the wave prefab
+        GameObject wave = Instantiate(WavePrefab, attackpo.position, Quaternion.identity);
+
+       
+    }
+}
