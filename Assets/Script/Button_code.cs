147caa3d7045fc58beb446f18ccf4f622a2055c6
Apisitkitti <apisit.o@mail.kmutt.ac.th>
2023-12-06 04:24:11 +0700

    Add main menu skill cooldown

---
+++ Assets/Script/Button_code.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button_code : MonoBehaviour
+{
+  public void SetExit()
+  {
+    Application.Quit();
+  }
+  public void ChangeScene()
+  {
+    SceneManager.LoadScene("art_test");
+  }
+}
