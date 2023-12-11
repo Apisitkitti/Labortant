8a06e4b5eb6a90e749cd907400b805bf1aee8178
Fennikz <tripop.xtarnos@mail.kmutt.ac.th>
2023-12-11 21:18:50 +0700

    edit: tuto

---
+++ Assets/Script/Goal_to_scene2.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal_to_scene2 : MonoBehaviour
+{
+  [SerializeField] GameObject Player;
+  // [SerializeField] GameObject Win;
+   void OnTriggerEnter2D(Collider2D col)
+   {
+    if(col.gameObject.tag == "Player")
+    {
+      Destroy(Player);
+      // Win.SetActive(true);
+      SceneManager.LoadScene("2");
+    }
+   }
+}
---
+++ Assets/Script/OnInteract.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnInteract : MonoBehaviour
+{
+    public GameObject tutorialUI;
+    public GameObject interactUI;
+
+    void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        if (collider2D.tag == "Player")
+        {
+            interactUI.SetActive(true);
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D collider2D)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            tutorialUI.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider2D)
+    {
+        if (collider2D.tag == "Player")
+        {
+            tutorialUI.SetActive(false);
+            interactUI.SetActive(false);
+        }
+    }
+}
